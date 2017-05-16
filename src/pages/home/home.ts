bfc803222291e6a104501a87a0e2216c5debadfb
Grinil Khanna <gmkhanna@gmail.com>
2017-05-16 16:14:32 -0700

    Add buttons, platform ios and android on exports, ion card to display perceived speech.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { SpeechRecognition } from '@ionic-native/speech-recognition';
-import { NavController } from 'ionic-angular';
+import { SpeechRecognition, SpeechRecognitionListeningOptionsAndroid, SpeechRecognitionListeningOptionsIOS } from '@ionic-native/speech-recognition';
+import { NavController, Platform } from 'ionic-angular';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'page-home',
@@ -8,8 +9,30 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(private speech: SpeechRecognition, public navCtrl: NavController) {
+  speechPercept: Array<string> = [];
+  androidOptions: SpeechRecognitionListeningOptionsAndroid;
+  iosOptions: SpeechRecognitionListeningOptionsIOS;
 
+  constructor(private speech: SpeechRecognition, public navCtrl: NavController, private platform: Platform) {
+
+  }
+
+  listenForSpeech():void {
+
+    this.androidOptions = {
+      prompt: "Aneeda..."
+    }
+
+    this.iosOptions = {
+      language: "en-US"
+    }
+
+    if(this.platform.is('android')) {
+      this.speech.startListening(this.androidOptions).subscribe(data => this.speechPercept = data, errors => console.log(errors))
+    }
+    else if (this.platform.is('ios')) {
+      this.speech.startListening(this.iosOptions).subscribe(data => this.speechPercept = data, errors => console.log(errors))
+    }
   }
 
   async isSpeechSupported():Promise<boolean> {
@@ -29,4 +52,26 @@ export class HomePage {
     }
   }
 
-}
+  async hasPermission():Promise<boolean> {
+    try {
+      const permission = await this.speech.hasPermission()
+      console.log(permission);
+      return permission;
+    }
+    catch(e) {
+      console.log(e);
+    }
+  }
+
+  async getSupportedLang():Promise<Array<string>> {
+    try {
+      const languages = await this.getSupportedLang();
+      console.log(languages);
+      return languages;
+    }
+    catch(e) {
+      console.log(e);
+    }
+  }
+
+  }
